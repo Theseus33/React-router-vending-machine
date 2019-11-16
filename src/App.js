a0c7f82e58d4b992322ba325fbc04cf4200c39dc
Rana Bhattacharyya <ranab33@gmail.com>
2019-11-15 21:43:58 -0500

    routes added

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import Chips from "./Chips";
 import Sardines from "./Sardines";
 import Soda from "./Soda";
 import VendingMachine from "./VendingMachine";
+import { Route } from "react-router-dom";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <VendingMachine />
-        <Chips />
-        <Sardines />
-        <Soda />
+        <Route exact path="/" render={() => <VendingMachine />} />
+        <Route exact path="/chips" render={() => <Chips />} />
+        <Route exact path="/sardines" render={() => <Sardines />} />
+        <Route exact path="/soda" render={() => <Soda />} />
       </div>
     );
   }
